805216fd81445ffd3b977f0939dac2f8f8f873e7
Linet <muthiigathoni@gmail.com>
2023-07-24 09:52:46 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    // Send fetch request to the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        // Set the dog image in the state
+        setDogImage(data.message);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? (
+        // If the dog image is available, show it
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        // If the dog image is not available yet, show "Loading..."
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
